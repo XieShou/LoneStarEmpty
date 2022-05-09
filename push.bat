3b9c49c10f2bfe071bddb0381edb40d97407e0bc
liaozuojia <liaozuojia@kuaishou.com>
2022-05-09 16:24:32 +0800

    初始化仓库

---
+++ push.bat
@@ -0,0 +1,27 @@
+::设置模块名字
+
+SET ToolName=upm-com.lonestar.empty
+
+::设置模块版本
+
+SET ToolVersion=1.0.0
+
+::设置模块源路径
+
+SET ToolAssetPath=Packages\com.lonestar.empty
+
+::此命令会创建一个ToolName的分支，并同步ToolAssetPath下的内容
+
+git subtree split --prefix=%ToolAssetPath% --branch %ToolName%
+
+:: 在ToolName分支设置标签ToolVersion节点
+
+git tag %ToolVersion% %ToolName%
+
+:: 推送到远端
+
+git push origin %ToolName% %ToolVersion%
+
+git push origin %ToolName%
+
+pause
